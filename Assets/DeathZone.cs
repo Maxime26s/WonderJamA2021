6cf51fe404b6d962909bb3b676e2d59bd116c10e
Maxime26s <35577093+Maxime26s@users.noreply.github.com>
2021-10-09 21:41:13 -0400

    RISING FEIEXD FIXED

--- Assets/DeathZone.cs
+++ Assets/DeathZone.cs
@@ -23,4 +23,13 @@ public class DeathZone : MonoBehaviour
         else if (collision.gameObject.TryGetComponent(out Rigidbody rb))
             Destroy(collision.gameObject);
     }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.TryGetComponent(out CharacterController characterController))
+            characterController.MoveToPachinko();
+        else if (other.gameObject.TryGetComponent(out Rigidbody rb))
+            Destroy(other.gameObject);
+    }
 }
