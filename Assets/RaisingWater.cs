5e994730d73c4cfa37983830fec94ba89489a5b5
Maxime26s <35577093+Maxime26s@users.noreply.github.com>
2021-10-09 21:38:47 -0400

    rising water

---
+++ Assets/RaisingWater.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaisingWater : MonoBehaviour
+{
+    public float timeBeforeRise, riseSpeed, timeRunning;
+    public bool rising;
+    public GameObject fakeWater;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeRunning = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!rising)
+        {
+            timeRunning += Time.deltaTime;
+            if (timeRunning > timeBeforeRise)
+                rising = true;
+        }
+        else if (rising)
+        {
+            fakeWater.transform.localScale += new Vector3(0, Time.deltaTime * riseSpeed, 0);
+            fakeWater.transform.position -= new Vector3(0, Time.deltaTime * riseSpeed * 0.5f, 0);
+            transform.parent.transform.position += new Vector3(0, Time.deltaTime * riseSpeed, 0);
+        }
+
+    }
+}
